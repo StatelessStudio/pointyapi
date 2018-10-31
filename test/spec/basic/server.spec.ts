cfc8f15c9405c7308d8c6caaf4ef4b71c19bcae8
Drew Immerman <drew@stateless.studio>
2018-10-31 01:37:16 -0400

    Create basic example spec

---
+++ test/spec/basic/server.spec.ts
@@ -0,0 +1,32 @@
+import 'jasmine';
+
+import { pointy } from '../../../src/';
+import { forkServer } from '../../../src/fork-server';
+const ROOT_PATH = require('app-root-path').toString();
+
+const http = pointy.http;
+
+let serverfork;
+
+beforeAll(async () => {
+	serverfork = await forkServer('./lib/test/examples/basic/server.js');
+
+	// Database
+	await pointy.db
+		.connect(ROOT_PATH)
+		.catch((error) =>
+			fail('Cannot start database' + JSON.stringify(error))
+		);
+});
+
+afterAll(() => {
+	if (serverfork) {
+		serverfork.kill();
+	}
+});
+
+describe('API Server', () => {
+	it('is running', async () => {
+		await http.get('/', {}, [ 200, 404 ]).catch((error) => fail(error));
+	});
+});
---
+++ test/spec/basic/user/user-delete.spec.ts
@@ -0,0 +1,4 @@
+import { pointy } from '../../../../src';
+const http = pointy.http;
+
+describe('User API Delete', () => {});
