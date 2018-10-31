5e90de29de6ff995762b1bcf4ddc5be20e9e6dd9
Drew Immerman <drew@stateless.studio>
2018-10-30 21:06:50 -0400

    Added enums index

---
+++ src/enums.ts
@@ -0,0 +1,18 @@
+/**
+ * # Enums
+ *
+ * There are 2 built-in enums, `UserRole` and `UserStatus`.
+ *
+ * You can create your own to replace these, as long as it
+ * has the required members:
+ * - UserRole
+ *  - Admin
+ * - UserStatus
+ *  - Active
+ */
+
+/**
+ * Enums
+ */
+export { UserRole } from './enums/user-role';
+export { UserStatus } from './enums/user-status';
