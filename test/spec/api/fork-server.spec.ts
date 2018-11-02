b54e2a363843ac9a68dc3b8a300347bc1d4a1248
Drew Immerman <drew@stateless.studio>
2018-11-01 22:34:17 -0400

    Removed forkServer() test from api

--- test/spec/api/fork-server.spec.ts
+++ test/spec/api/fork-server.spec.ts
@@ -1,10 +1,1 @@
-import { forkServer } from '../../../src/fork-server';
-
-describe('forkServer', () => {
-	it('starts a server', async () => {
-		let serverResult: any = false;
-		const server = await forkServer('./lib/test/examples/basic/server.js')
-			.then((result) => (serverResult = result))
-			.catch((error) => fail(error));
-	});
-});
+// Tested in basic example
