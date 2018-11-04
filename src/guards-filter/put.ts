e205b86cc338caac5c1cf925eb28c96dfc45dfcc
Drew Immerman <drew@stateless.studio>
2018-11-04 16:32:20 -0500

    Created put filter

--- src/guards-filter/put.ts
+++ src/guards-filter/put.ts
@@ -1,9 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
+import { writeFilter } from '../bodyguard/write-filter';
 
 export function putFilter(
 	request: Request,
 	response: Response,
 	next: NextFunction
 ) {
+	const writeFilterResult = writeFilter(
+		request.body,
+		request.user,
+		request.payloadType,
+		request.userType
+	);
+
+	if (writeFilterResult === true) {
+		next();
+	}
+	else {
+		response.forbiddenResponder(
+			'Cannot write member ' + writeFilterResult,
+			response
+		);
+	}
+
 	next();
 }
