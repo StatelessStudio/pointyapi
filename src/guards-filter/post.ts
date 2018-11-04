a4883d9404d11324dc30304740ae9c354bc82f55
Drew Immerman <drew@stateless.studio>
2018-11-04 16:32:06 -0500

    Created postFilter

--- src/guards-filter/post.ts
+++ src/guards-filter/post.ts
@@ -1,9 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
+import { writeFilter } from '../bodyguard/write-filter';
 
 export function postFilter(
 	request: Request,
 	response: Response,
 	next: NextFunction
 ) {
-	next();
+	// Posting new user is always self
+	let isSelfResult;
+
+	if (request.method === 'POST' && request.payloadType === request.userType) {
+		isSelfResult = true;
+	}
+
+	const writeFilterResult = writeFilter(
+		request.body,
+		request.user,
+		request.payloadType,
+		request.userType,
+		isSelfResult
+	);
+
+	if (writeFilterResult === true) {
+		next();
+	}
+	else {
+		response.forbiddenResponder(
+			'Cannot write member ' + writeFilterResult,
+			response
+		);
+	}
 }
