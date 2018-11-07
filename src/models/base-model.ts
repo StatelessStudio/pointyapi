3ff72e4c512f2c4e409794072052625b22165786
Drew Immerman <drew@stateless.studio>
2018-11-07 10:19:48 -0500

    Added constructor to BaseModel class to set id

--- src/models/base-model.ts
+++ src/models/base-model.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 
 export class BaseModel {
 	public id?: any = undefined;
+
+	constructor(id?) {
+		this.id = id;
+	}
 }
 
 export interface BaseModelInterface {
