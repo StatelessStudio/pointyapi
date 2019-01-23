4e72afe424bf507e2a3df8af9b37aaacc3f7b20a
Drew Immerman <drewimmerman@gmail.com>
2019-01-23 10:02:44 -0500

    [Issue #90] Add CORS headers to basicCors()

--- src/middleware/basic-cors.ts
+++ src/middleware/basic-cors.ts
@@ -6,11 +6,13 @@ import { Request, Response, NextFunction } from 'express';
  * 	Access-Control-Allow-Origin
  * 		'*'
  * 	Access-Control-Allow-Methods
- * 		'POST, GET, PUT, DELETE'
+ * 		'POST, GET, PUT, DELETE, OPTIONS'
  * 	Access-Control-Allow-Headers
- * 		'X-Requested-With, Content-Type, Authorization'
+ * 		'X-Requested-With, Content-Type, Authorization, Cache-Control'
  * 	Access-Control-Allow-Credentials
  * 		'true'
+ *  Content-Type
+ *		'application/json;charset=utf-8'
  *
  */
 export function basicCors(
@@ -21,13 +23,14 @@ export function basicCors(
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	response.setHeader(
 		'Access-Control-Allow-Methods',
-		'POST, GET, PUT, DELETE'
+		'POST, GET, PUT, DELETE, OPTIONS'
 	);
 	response.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-Requested-With, Content-Type, Authorization'
+		'X-Requested-With, Content-Type, Authorization, Cache-Control'
 	);
 	response.setHeader('Access-Control-Allow-Credentials', 'true');
+	response.setHeader('Content-Type', 'application/json;charset=utf-8');
 
 	next();
 }
