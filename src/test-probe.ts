c6f8165de9a3f8245819b4bddcf19e3cc7931436
Drew Immerman <drewimmerman@gmail.com>
2019-01-29 03:15:24 -0500

    Added createMockRequest()

--- src/test-probe.ts
+++ src/test-probe.ts
@@ -1,3 +1,5 @@
 export { coreTestProbe } from './test-probe/core-test-probe';
 export { requestTestProbe } from './test-probe/request-test-probe';
 export { userTestProbe } from './test-probe/user-test-probe';
+
+export { createMockRequest } from './test-probe/create-mock-request';
---
+++ src/test-probe/create-mock-request.ts
@@ -0,0 +1,63 @@
+import { mockRequest, mockResponse } from 'mock-req-res';
+import { getRepository } from 'typeorm';
+import { BaseUser } from '../models/base-user';
+
+/**
+ * Create mock request/response objects
+ */
+export function createMockRequest(
+	method: string = 'GET',
+	url: string = '/api/v1/user'
+) {
+	// Create request
+	const request = mockRequest();
+	request.repository = getRepository(BaseUser);
+	request.method = method;
+	request.baseUrl = url;
+	request.userType = BaseUser;
+	request.payloadType = BaseUser;
+	request.joinMembers = [];
+	request.identifier = 'id';
+
+	// Create response
+	const response = mockResponse();
+
+	// Conflict responder
+	response.conflictResponder = (error) =>
+		fail('Conflict: ' + JSON.stringify(error));
+
+	// Delete responder
+	response.deleteResponder = (result) =>
+		fail('Delete: ' + JSON.stringify(result));
+
+	// Forbidden responder
+	response.forbiddenResponder = (error) =>
+		fail('Forbidden: ' + JSON.stringify(error));
+
+	// Get responder
+	response.getResponder = (result) => fail('Get: ' + JSON.stringify(result));
+
+	// Error responder
+	response.error = (error) => fail('Error: ' + JSON.stringify(error));
+
+	// Gone responder
+	response.goneResponder = (error) => fail('Gone: ' + JSON.stringify(error));
+
+	// Post responder
+	response.postResponder = (result) =>
+		fail('Post: ' + JSON.stringify(result));
+
+	// Put responder
+	response.putResponder = (result) => fail('Put: ' + JSON.stringify(result));
+
+	// Unauthorized responder
+	response.unauthorizedResponder = (error) =>
+		fail('Unauthorized: ' + JSON.stringify(error));
+
+	// Validation responder
+	response.validationResponder = (error) =>
+		fail('Validation: ' + JSON.stringify(error));
+
+	// Return request/response
+	return { request, response };
+}
