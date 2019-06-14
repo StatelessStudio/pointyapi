117f39bf499745b0876222734e7cd74e0b2004d8
Drew Immerman <drewimmerman@gmail.com>
2019-06-14 10:46:10 -0400

    Removed database error handler

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -14,60 +14,68 @@ export class PointyPostgres extends BaseDb {
 	 * 	a string to load from file, or pass the object directly).  Database
 	 * 	will rely on `process.env.DATABASE_URL` if this is not set.
 	 */
-	public async connect(options?: string | Object): Promise<Connection> {
-		let pgOptions: any;
+	public connect(options?: string | Object): Promise<Connection> {
+		return new Promise(async (accept, reject) => {
+			let pgOptions: any;
 
-		if (process.env.DATABASE_URL) {
-			// Live
-			pgOptions = PostgressConnectionStringParser.parse(
-				process.env.DATABASE_URL
-			);
-			pgOptions.type = 'postgres';
+			if (process.env.DATABASE_URL) {
+				// Live
+				pgOptions = PostgressConnectionStringParser.parse(
+					process.env.DATABASE_URL
+				);
+				pgOptions.type = 'postgres';
 
-			this.logger('Using production database');
-			this.logger(
-				'Using database driver',
-				process.env.TYPEORM_DRIVER_TYPE || pgOptions.type
-			);
-		}
-		else {
-			// Local
-			if (typeof options === 'string') {
-				pgOptions = require(path.join(
-					options.toString(),
-					'local.config.json'
-				));
+				this.logger('Using production database');
+				this.logger(
+					'Using database driver',
+					process.env.TYPEORM_DRIVER_TYPE || pgOptions.type
+				);
 			}
 			else {
-				pgOptions = options;
+				// Local
+				if (typeof options === 'string') {
+					pgOptions = require(path.join(
+						options.toString(),
+						'local.config.json'
+					));
+				}
+				else {
+					pgOptions = options;
+				}
+
+				this.logger('Using development database');
+				this.logger(
+					'Using database driver',
+					process.env.TYPEORM_DRIVER_TYPE || pgOptions.type
+				);
+
+				this.shouldSync = true;
 			}
 
-			this.logger('Using development database');
-			this.logger(
-				'Using database driver',
-				process.env.TYPEORM_DRIVER_TYPE || pgOptions.type
-			);
+			// Create connection
+			const conn = await createConnection(<ConnectionOptions>{
+				name: this.connectionName,
+				type: process.env.TYPEORM_DRIVER_TYPE || pgOptions.type,
+				driver: process.env.TYPEORM_DRIVER_TYPE || pgOptions.type,
+				host: pgOptions.host,
+				port: pgOptions.port,
+				username: pgOptions.user,
+				password: pgOptions.password,
+				database: pgOptions.database,
+				entities: this.entities,
+				synchronize: this.shouldSync,
+				uuidExtension: pgOptions.uuidExtension
+					? pgOptions.uuidExtension
+					: 'pgcrypto'
+			}).catch((error) => {
+				reject(error);
+				return false;
+			});
 
-			this.shouldSync = true;
-		}
-
-		// Create connection
-		this.conn = await createConnection(<ConnectionOptions>{
-			name: this.connectionName,
-			type: process.env.TYPEORM_DRIVER_TYPE || pgOptions.type,
-			driver: process.env.TYPEORM_DRIVER_TYPE || pgOptions.type,
-			host: pgOptions.host,
-			port: pgOptions.port,
-			username: pgOptions.user,
-			password: pgOptions.password,
-			database: pgOptions.database,
-			entities: this.entities,
-			synchronize: this.shouldSync,
-			uuidExtension: pgOptions.uuidExtension
-				? pgOptions.uuidExtension
-				: 'pgcrypto'
-		}).catch((error) => this.errorHandler(error));
-
-		return this.conn;
+			if (conn && conn instanceof Connection) {
+				this.conn = conn;
+				accept(this.conn);
+			}
+		});
 	}
 }
