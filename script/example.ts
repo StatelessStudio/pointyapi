a485106e624a2de1cab83d6d0369a0a06c501266
Drew Immerman <drewimmerman@gmail.com>
2022-02-18 21:45:47 -0500

    Fix compilation errors

--- script/example.ts
+++ script/example.ts
@@ -1,17 +1,9 @@
-// The following imports are generic across all scripts
-import { bootstrap } from '../src/bootstrap';
-
-// These imports are specific to the example script, but may be used
-// 	for others
-import { log } from '../src/log';
 import { env } from '../src/environment';
 
 /**
  * Do something!
  */
 export async function example(): Promise<void> {
-	log.info('App Name: ', env.APP_TITLE);
-	log.warn('Environment: ', env.NODE_ENV);
+	console.log(env.APP_TITLE);
+	console.log(env.NODE_ENV);
 }
-
-bootstrap(example);
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { log } from './log';
-import { env } from './environment';
-
-/**
- * Start your application in the main() function
- */
-export async function main(): Promise<void> {
-	log.info('Hello ' + env.APP_TITLE);
-}
