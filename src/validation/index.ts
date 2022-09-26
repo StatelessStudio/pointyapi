7d4871c2427b7cfd9f1e0144cd9abdd10afc3836
Drew Immerman <drewimmerman@gmail.com>
2022-09-26 14:30:53 -0400

    Add documentation and tests for validateAllowingStrings()

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,5 +1,11 @@
-import { validateSync } from 'class-validator';
+import { validateSync, ValidationError } from 'class-validator';
 
+/**
+ * Check if a string is an integer (signed/unsigned)
+ *
+ * @param str String to check
+ * @returns Returns true if the string is int
+ */
 export function isIntString(str: string): boolean {
 	return (
 		typeof str === 'string' &&
@@ -7,12 +13,18 @@ export function isIntString(str: string): boolean {
 	);
 }
 
-export function validateAllowingStrings(obj: object) {
-	let validationErrors = validateSync(obj, {
+/**
+ * Run validation on a model; allowing ints as strings
+ *
+ * @param obj Object to check
+ * @returns Returns true if the string is int
+ */
+export function validateAllowingStrings(obj: object): ValidationError[] {
+	const validationErrors = validateSync(obj, {
 		skipMissingProperties: true
 	});
 
-	validationErrors = validationErrors.filter(error => {
+	return validationErrors.filter(error => {
 		const constraints = error.constraints;
 
 		if (
@@ -24,6 +36,4 @@ export function validateAllowingStrings(obj: object) {
 
 		return Object.keys(error.constraints).length;
 	});
-
-	return validationErrors;
 }
